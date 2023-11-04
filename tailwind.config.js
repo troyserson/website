6a7a22bc7f6c123516dae2ccbe9839328ad878b7
tannertanner1 <tannerserson@gmail.com>
2023-11-04 08:11:05 -0700

    removed unused packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   // darkMode: 'class',
   content: [
     "./pages/**/*.{js,ts,md,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,md,jsx,tsx,mdx}",
-    "./posts/**/*.{js,ts,md,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
-      // typography: {
-      typography: ({ theme }) => ({
-        DEFAULT: {
-          css: {
-            'img': {
-              display: 'block',
-              float: 'none',
-              // marginLeft: 'auto'
-              // marginRight: 'auto'
-              margin: 'auto',
-              width: 'auto',
-              height: 'auto',
-              maxWidth: '50%', // makes images responsive by default
-              maxHeight: 'auto', // allows images to maintain their aspect ratio
-            },
-            // changes color of list numbers
-            '--tw-prose-counters': theme('colors.zinc[500]'),
-            '--tw-prose-invert-counters': theme('colors.zinc[400]'),
-            // changes color of list bullets
-            '--tw-prose-bullets': theme('colors.zinc[500]'),
-            '--tw-prose-invert-bullets': theme('colors.zinc[400]'),
-            // https://github.com/tailwindlabs/tailwindcss-typography/blob/master/src/styles.js
-          },
-        },
+      // DEFAULT: {
+      //   css: {
+      //     'img': {
+      //       display: 'block',
+      //       float: 'none',
+      //       // marginLeft: 'auto'
+      //       // marginRight: 'auto'
+      //       margin: 'auto',
+      //       width: 'auto',
+      //       height: 'auto',
+      //       maxWidth: '100%', // makes images responsive by default
+      //       maxHeight: 'auto', // allows images to maintain their aspect ratio
+      //     }
+      //   }
       // },
-      }),
       colors: {
         // 'lite': 'white',
         // 'dark': 'zinc-900',
@@ -54,11 +43,10 @@ module.exports = {
       fontFamily: {
         rubik: ['Rubik', 'sans-serif'],
       },
-
       keyframes: { animatedgradient: { '0%': { backgroundPosition: '0% 50%' }, '50%': { backgroundPosition: '100% 50%' }, '100%': { backgroundPosition: '0% 50%' } }},
       backgroundSize: { '300%': '300%' },
       animation: { gradient: 'animatedgradient 6s ease infinite alternate' }
     }
   },
-  plugins: [require('@tailwindcss/typography')]
+  plugins: []
 }
